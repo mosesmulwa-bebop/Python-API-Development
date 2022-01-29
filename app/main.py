c4d430d89e5fc12d3a75f81c00c5f2edf5d6d8f1
Moses Mulwa <mosesmulwa123@gmail.com>
2022-01-29 22:59:33 +0300

    add phone number

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from . import models
-from .database import engine
+#from . import models
+#from .database import engine
 from .routers import post,user,auth,vote
-from .config import settings
+#from .config import settings
 
 
-models.Base.metadata.create_all(bind = engine)
+#models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
 
