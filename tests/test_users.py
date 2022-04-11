f410b9be9a83274d226a24d85341c3b4fcd7e20f
Moses Mulwa <mosesmulwa123@gmail.com>
2022-04-11 16:34:51 +0300

    added testing users

--- tests/test_users.py
+++ tests/test_users.py
@@ -7,3 +7,10 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'message':"Hello world!!!"}
+
+def test_create_user():
+    #request = email and password
+    res = client.post("/users/", json={"email": "stuff2@gmail.com", "password": "stuff"})
+    assert res.status_code == 201
+    print(res.json())
+    #response = email and created at
